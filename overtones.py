c5ff42a8bafa09bf37b18631e648184a5b9fa781
William Lu <william.a.lu@gmail.com>
2015-05-01 11:36:19 -0400

    real-time graphing of power vs. frequency

--- overtones.py
+++ overtones.py
@@ -1,82 +1,78 @@
 import pyaudio
 import numpy as np
 from matplotlib import pyplot as plt
-from pprint import pprint
+import time
 
-CHUNKSIZE = 1024 # fixed chunk size
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-RECORD_SECONDS = 5
+def read_audio(RECORD_SECONDS, RATE):
+    """
+    Reads audio from microphone and stores input in a list
+    Output: frames (list)
+    """
 
-# initialize portaudio
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, 
-				channels=CHANNELS, 
-				rate=RATE, 
-				input=True, 
-				frames_per_buffer=CHUNKSIZE)
+    # sets standard parameters for what normally would go into a WAV file
+    # seconds to record and record rate are already set by user
+    CHUNKSIZE = 1024 # fixed chunk size
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
 
-np.set_printoptions(threshold=np.nan)
-frames = []
-for i in range(0, int(RATE / CHUNKSIZE * RECORD_SECONDS)):
-	data = stream.read(CHUNKSIZE)
-	# pprint(np.fromstring(data, dtype=np.int16))
-	frames.append(np.divide(np.fromstring(data, dtype=np.int16), 1024.0))
+    # initialize portaudio
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16, 
+                    channels=CHANNELS, 
+                    rate=RATE, 
+                    input=True, 
+                    frames_per_buffer=CHUNKSIZE)
 
-# print len(frames)
-# print len(frames[0])
-# for i in range(len(frames)):
-	# print sum(frames[i]) / len(frames[i])
-numpydata = np.hstack(frames)
-numpydata = np.fft.fft(numpydata)
-freq = np.fft.fftfreq(numpydata.shape[-1])
-print numpydata.shape[-1]
+    frames = []
 
-# plot data
-plt.plot(freq, numpydata)
-plt.show()
+    # read audio from microphone, turn it into a string, store it in a list of frames
+    for i in range(0, int(RATE / CHUNKSIZE * RECORD_SECONDS)):
+        data = stream.read(CHUNKSIZE)
+        frames.append(np.divide(np.fromstring(data, dtype=np.int16), 1024.0))
 
-# close stream
-stream.stop_stream()
-stream.close()
-p.terminate()
+    # close stream
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    
+    return frames
 
-# import pyaudio
-# import wave
+def spectrum_plot():
+    """
+    Uses read_audio to take in microphone input, runs fft, generates power vs. frequency graphs
+    in real time.
+    """
+    
+    # initialize matplotlib graphs, ion allows for interactive graphing
+    plt.ion()
+    plt.show()
 
-# CHUNK = 1024
-# FORMAT = pyaudio.paInt16
-# CHANNELS = 2
-# RATE = 44100
-# RECORD_SECONDS = 5
-# WAVE_OUTPUT_FILENAME = "output.wav"
+    RECORD_SECONDS = 0.03
+    RATE = 44100
+    
+    for i in range(1000):
+        
+        # clear plot, set axis limits
+        plt.clf()
+        plt.ylim((0, 10000))
+        plt.xlim((0, 5000))
+        
+        numpydata = np.hstack(read_audio(RECORD_SECONDS, RATE))
 
-# p = pyaudio.PyAudio()
+        n = numpydata.size
 
-# stream = p.open(format=FORMAT,
-#                 channels=CHANNELS,
-#                 rate=RATE,
-#                 input=True,
-#                 frames_per_buffer=CHUNK)
+        spectrum = np.fft.fft(numpydata)
+        freq = np.fft.fftfreq(n, 1./RATE)
 
-# print("* recording")
+        # plots data
+        # x axis is frequency (hz)
+        # y axis is power
+        plt.plot(freq, np.absolute(spectrum))
+        plt.draw()
+        i += 1
+        # time.sleep(0.05)
+    
+    plt.close()
 
-# frames = []
-
-# for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-#     data = stream.read(CHUNK)
-#     frames.append(data)
-
-# print("* done recording")
-
-# stream.stop_stream()
-# stream.close()
-# p.terminate()
-
-# wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-# wf.setnchannels(CHANNELS)
-# wf.setsampwidth(p.get_sample_size(FORMAT))
-# wf.setframerate(RATE)
-# wf.writeframes(b''.join(frames))
-# wf.close()
+if __name__ == "__main__":
+    spectrum_plot()
